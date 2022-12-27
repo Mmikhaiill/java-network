ebf1b14188a3c1ef12762a4cea33637b8dfbc2e0
Mike <email@mail.ru>
2022-12-27 03:18:47 +0300

    lesson6 home work

---
+++ lesson6/server-netty/src/main/java/org/example/netty/Server.java
@@ -0,0 +1,89 @@
+package org.example.netty;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Server {
+
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ServerChannelInitializer());
+
+            ChannelFuture f = b.bind(PORT).sync();
+
+            f.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+
+class ServerChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
+
+        pipeline.addLast( new ServerHandler());
+    }
+}
+
+@ChannelHandler.Sharable
+class ServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws UnknownHostException {
+        ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\n");
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, String msg) {
+        String[] command = msg.split(":");
+
+        String response;
+        if (command[1].length() == Integer.parseInt(command[0])) {
+            response = "2:ок";
+        } else {
+            response =  "3:err";
+        }
+
+        ctx.writeAndFlush("-> " + response + '\n');
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
